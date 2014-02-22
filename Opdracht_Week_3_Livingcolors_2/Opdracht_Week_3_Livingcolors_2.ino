f6aaf29add0154f34fe8b40b3cfe476c6e51eafd
misteriepie@gmail.com <misteriepie@gmail.com@4dbe536f-b5d7-5901-bcb5-1e80133ea507>
2014-02-22 21:53:07 +0000

    loop aan gepast en functiew gemaakt voor togglebutton

--- Opdracht_Week_3_Livingcolors_2/Opdracht_Week_3_Livingcolors_2.ino
+++ Opdracht_Week_3_Livingcolors_2/Opdracht_Week_3_Livingcolors_2.ino
@@ -3,58 +3,102 @@
 RGBcolor color;    // maak een instantie aan van de struct
 state colorState;  // maak een instantie aan van de Enum
 
-boolean Button;
-
 void setup()
 {
+  Serial.begin(9600);
+  pinMode(BUTTONPIN,INPUT);
+  colorState=stateSetRedColor;
 }
 
 void loop()
-{
-  if (!Button)
+{  
+  if (toggleButton(BUTTONPIN))
   {
     if (colorState==stateSetRedColor)
     {
-      if (color.red <255)
+     color.red=0;
+     color.green=255;
+     color.blue=255;
+      writeAnalogOutputs(&color);
+      colorState=stateGoToYellow;
+      Serial.println(colorState);
+    }
+    else if(colorState==stateGoToYellow)
+    {
+      if (color.green >0)
+      {
+        color.green--;
+        writeAnalogOutputs(&color);
+      }
+      else
+      {
+        colorState=stateGoToGreen;
+      }
+    }
+    else if(colorState==stateGoToGreen)
+    {
+      if(color.red < 255)
       {
         color.red++;
         writeAnalogOutputs(&color);
       }
-    }
-    else if(colorState==stateGoToYellow)
-    {
-    }
-    else if(colorState==stateGoToGreen)
-    {
+      else 
+      {
+        colorState=stateGoToCyane;
+      }
     }
     else if(colorState==stateGoToCyane)
     {
+      if (color.blue >0)
+      {
+        color.blue--;
+        writeAnalogOutputs(&color);
+      }
+      else
+      {
+        colorState=stateGoToBLue;
+      }
     }
     else if(colorState==stateGoToBLue)
     {
+      if(color.green < 255)
+      {
+        color.green++;
+        writeAnalogOutputs(&color);
+      }
+      else
+      {
+        colorState=stateGoToMagenta;
+      }
     }
     else if(colorState==stateGoToMagenta)
     {
+      if (color.red >0)
+      {
+        color.red--;
+        writeAnalogOutputs(&color);
+      }
+      else
+      {
+        colorState=stateGoToRed;
+      }
     }
     else if(colorState==stateGoToRed)
     {
+      if(color.blue < 255)
+      {
+        color.blue++;
+        writeAnalogOutputs(&color);
+      }
+      else
+      {
+        colorState=stateGoToYellow;
+      }
     }
   } 
 }
 void writeAnalogOutputs( RGBcolor *c) // *c is een verwijzing naar c van het type RGBcolor.
 {
-  button = digitalRead(BUTTONPIN);  // pauze button
-
-    if( button != buttonState)
-  {
-    delay(DELAYTIME);
-    if ( button == HIGH)
-    {
-      pauze ++;
-    }
-  }
-  buttonState = button;
-
   if (DEBUG==1)
   {
     Serial.print("-");
@@ -69,3 +113,27 @@ void writeAnalogOutputs( RGBcolor *c) // *c is een verwijzing naar c van het typ
   analogWrite(RGBPINBLUE,c->blue);
   delay(DELAYTIME); 
 }
+
+boolean toggleButton (int buttonPinNumber )
+{
+  static boolean buttonState = HIGH;
+  static boolean buttonStatePrevious =LOW;
+  long time = 0;         // the last time the output pin was toggled
+  long debounce = 200;   // the debounce time, increase if the output flickers
+  boolean button = digitalRead(buttonPinNumber);  // pauze button
+  if (button==HIGH && buttonStatePrevious == LOW && millis() - time > debounce)
+  {
+    if(buttonState==HIGH)
+    {
+      buttonState=LOW;
+    }
+    else
+    {
+      buttonState=HIGH;
+    }
+    time = millis();
+  }
+  buttonStatePrevious = button;
+  return buttonState;
+}
+
