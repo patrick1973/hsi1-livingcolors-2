c4dfaec019686768155abb6b973801dbafcf8742
misteriepie@gmail.com <misteriepie@gmail.com@4dbe536f-b5d7-5901-bcb5-1e80133ea507>
2014-02-22 10:26:40 +0000

    first start

---
+++ trunk/Opdracht_Week_3_Livingcolors_2/Types.h
@@ -0,0 +1,39 @@
+/*----------------------------------------------------*/
+/* aut.     :  Patrick & Nikita                       */
+/* datum    :  20-02-2014                             */
+/* Project  :  Living Colors part 2                   */
+/*----------------------------------------------------*/
+
+
+#ifndef TYPES_H   
+#define TYPES_H
+
+#define DEBUG 0
+
+typedef enum RGBstate
+{
+  stateSetRedColor,
+  stateGoToYellow,
+  stateGoToGreen,
+  stateGoToCyane,
+  stateGoToBLue,
+  stateGoToMagenta,
+  stateGoToRed
+}state;
+
+typedef struct RGB
+{
+  int red;
+  int green;
+  int blue;
+}RGBcolor;
+
+const int RGBPINRED   = 9;  //PWM
+const int RGBPINGREEN = 10; //PWM
+const int RGBPINBLUE  = 11; //PWM
+const int LEDPINOUT   = 13; // used for testing the toggle function of the push button
+
+const int BUTTONPIN   = 2;  //Input pin for pauze button
+const int DELAYTIME   = 40;
+
+#endif TYPES_H
