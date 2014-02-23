da3878e3349188583db30749780a1b8095cab276
misteriepie@gmail.com <misteriepie@gmail.com@4dbe536f-b5d7-5901-bcb5-1e80133ea507>
2014-02-23 12:31:36 +0000

    programma devided in several modules

--- Opdracht_Week_3_Livingcolors_2/Opdracht_Week_3_Livingcolors_2.ino
+++ Opdracht_Week_3_Livingcolors_2/Opdracht_Week_3_Livingcolors_2.ino
@@ -1,3 +1,9 @@
+/*----------------------------------------------------*/
+/* aut.     :  Patrick & Nikita                       */
+/* datum    :  22-02-2014                             */
+/* Project  :  Living Colors part 2                   */
+/* Main program module                                */
+/*----------------------------------------------------*/
 #include "Types.h"
 
 RGBcolor color;    // maak een instantie aan van de struct
@@ -12,133 +18,15 @@ void setup()
 
 void loop()
 {  
-  rgbDemo();
-}
-void rgbDemo()
-{
   if (toggleButton(BUTTONPIN))
   {
-    if (colorState==stateSetRedColor)
-    {
-     color.red=0;
-     color.green=255;
-     color.blue=255;
-      writeAnalogOutputs(&color);
-      colorState=stateGoToYellow;
-      Serial.println(colorState);
-    }
-    else if(colorState==stateGoToYellow)
-    {
-      if (color.green >0)
-      {
-        color.green--;
-        writeAnalogOutputs(&color);
-      }
-      else
-      {
-        colorState=stateGoToGreen;
-      }
-    }
-    else if(colorState==stateGoToGreen)
-    {
-      if(color.red < 255)
-      {
-        color.red++;
-        writeAnalogOutputs(&color);
-      }
-      else 
-      {
-        colorState=stateGoToCyane;
-      }
-    }
-    else if(colorState==stateGoToCyane)
-    {
-      if (color.blue >0)
-      {
-        color.blue--;
-        writeAnalogOutputs(&color);
-      }
-      else
-      {
-        colorState=stateGoToBLue;
-      }
-    }
-    else if(colorState==stateGoToBLue)
-    {
-      if(color.green < 255)
-      {
-        color.green++;
-        writeAnalogOutputs(&color);
-      }
-      else
-      {
-        colorState=stateGoToMagenta;
-      }
-    }
-    else if(colorState==stateGoToMagenta)
-    {
-      if (color.red >0)
-      {
-        color.red--;
-        writeAnalogOutputs(&color);
-      }
-      else
-      {
-        colorState=stateGoToRed;
-      }
-    }
-    else if(colorState==stateGoToRed)
-    {
-      if(color.blue < 255)
-      {
-        color.blue++;
-        writeAnalogOutputs(&color);
-      }
-      else
-      {
-        colorState=stateGoToYellow;
-      }
-    }
-  } 
-  
-}
-void writeAnalogOutputs( RGBcolor *c) // *c is een verwijzing naar c van het type RGBcolor.
-{
-  if (DEBUG==1)
-  {
-    Serial.print("-");
-    Serial.print(c->red);
-    Serial.print("-");
-    Serial.print(c->green);
-    Serial.print("-");
-    Serial.println(c->blue); 
+    rgbDemo();
   }
-  analogWrite(RGBPINRED,c->red);
-  analogWrite(RGBPINGREEN,c->green);
-  analogWrite(RGBPINBLUE,c->blue);
-  delay(DELAYTIME); 
 }
 
-boolean toggleButton (int buttonPinNumber )
-{
-  static boolean buttonState = HIGH;
-  static boolean buttonStatePrevious =LOW;
-  long time = 0;         
-  long debounce = 200; 
-  boolean button = digitalRead(buttonPinNumber); 
-  if (button==HIGH && buttonStatePrevious == LOW && millis() - time > debounce)
-  {
-    if(buttonState==HIGH)
-    {
-      buttonState=LOW;
-    }
-    else
-    {
-      buttonState=HIGH;
-    }
-    time = millis();
-  }
-  buttonStatePrevious = button;
-  return buttonState;
-}
+
+
+
+
+
 
---
+++ Opdracht_Week_3_Livingcolors_2/ioHandling.ino
@@ -0,0 +1,45 @@
+/*----------------------------------------------------*/
+/* aut.     :  Patrick & Nikita                       */
+/* datum    :  22-02-2014                             */
+/* Project  :  Living Colors part 2                   */
+/* functions that are related to the IO               */
+/*----------------------------------------------------*/
+void writeAnalogOutputs( RGBcolor *c) // *c is een verwijzing naar c van het type RGBcolor.
+{
+  if (DEBUG==1)
+  {
+    Serial.print("-");
+    Serial.print(c->red);
+    Serial.print("-");
+    Serial.print(c->green);
+    Serial.print("-");
+    Serial.println(c->blue); 
+  }
+  analogWrite(RGBPINRED,c->red);
+  analogWrite(RGBPINGREEN,c->green);
+  analogWrite(RGBPINBLUE,c->blue);
+  delay(DELAYTIME); 
+}
+
+boolean toggleButton (int buttonPinNumber )
+{
+  static boolean buttonState = HIGH;
+  static boolean buttonStatePrevious =LOW;
+  long time = 0;         
+  long debounce = 200; 
+  boolean button = digitalRead(buttonPinNumber); 
+  if (button==HIGH && buttonStatePrevious == LOW && millis() - time > debounce)
+  {
+    if(buttonState==HIGH)
+    {
+      buttonState=LOW;
+    }
+    else
+    {
+      buttonState=HIGH;
+    }
+    time = millis();
+  }
+  buttonStatePrevious = button;
+  return buttonState;
+}
